b1148d0317354a4dca106847f2ded4983cb5f3c4
Rohit Vipin Mathews <rohitvipin@gmail.com>
2016-12-17 14:00:57 +0530

    head

--- XFPDFMerge/XFPDFMerge/App.xaml.cs
+++ XFPDFMerge/XFPDFMerge/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace XFPDFMerge
 {
@@ -13,7 +8,7 @@ namespace XFPDFMerge
         {
             InitializeComponent();
 
-            MainPage = new XFPDFMerge.MainPage();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
